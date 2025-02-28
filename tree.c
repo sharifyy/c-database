61df5ef600eaeebaa3d4c01663881056c58d986a
Mohamad <sharifyy@gmail.com>
2025-02-28 18:46:28 +0330

    added data structures

--- tree.c
+++ tree.c
@@ -1,7 +1,16 @@
 #include "tree.h"
 
+Tree root = {
+    .n = {
+        .tag = (TagRoot | TagNode),
+        .north = (Node *) &root,
+        .west = 0,
+        .east = 0,
+        .path = "/"
+    }
+};
 
 int main(void) {
-    printf("Hello, World!\n");
+    printf("%p\n", &root);
     return 0;
 }
--- tree.h
+++ tree.h
@@ -9,3 +9,39 @@
 #include <assert.h>
 #include <errno.h>
 
+#define TagRoot 1 /* 00 01 */
+#define TagNode 2 /* 00 10 */
+#define TagLeaf 4 /* 01 00 */
+
+typedef unsigned int int32;
+typedef unsigned short int int16;
+typedef unsigned char int8;
+
+typedef unsigned char Tag;
+
+
+struct s_node {
+    Tag tag;
+    struct s_node *north;
+    struct s_node *west;
+    struct s_leaf *east;
+    int8 path[256];
+};
+typedef struct s_node Node;
+
+
+struct s_leaf {
+    Tag tag;
+    union u_tree *west;
+    struct  s_leaf *east;
+    int8 key[128];
+    int8 *value;
+    int16 size;
+};
+typedef struct s_leaf Leaf;
+
+union u_tree {
+    Node n;
+    Leaf l;
+};
+typedef union u_tree Tree;
