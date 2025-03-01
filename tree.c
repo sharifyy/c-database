1cbfe672d8bd95d7e4da4dc4929624916ee684d9
Mohamad <sharifyy@gmail.com>
2025-03-01 15:49:01 +0330

    creat node

--- tree.c
+++ tree.c
@@ -10,7 +10,40 @@ Tree root = {
     }
 };
 
+void zero(int8 *str, int16 size) {
+    int8 *p;
+    int16 n;
+    for (n = 0, p = str; n < size; p++, n++) {
+        *p = 0;
+    }
+}
+
+Node *create_node(Node *parent, int8 *path) {
+    Node *n;
+    int16 size;
+
+    assert(parent);
+    size = sizeof(struct s_node);
+    n = (Node *) malloc((int)size);
+    zero((int8 *) n, size);
+
+    parent->west = n;
+    n->tag = TagNode;
+    n->north = parent;
+    strncpy((char *)n->path,(char *)path,255);
+
+    return n;
+}
+
 int main(void) {
-    printf("%p\n", &root);
+    Node *n, *n2;
+    n = create_node((Node *)&root, (int8 *)"/Users");
+    assert(n);
+    n2 = create_node(n, (int8 *)"/Users/login");
+    assert(n2);
+
+    printf("%p %p\n", n,n2);
+    free(n2);
+    free(n);
     return 0;
 }
